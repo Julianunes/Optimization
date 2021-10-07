3fb359e33afc8d65a1f561bf1d7a115f2d38df57
Julianunes <43185362+Julianunes@users.noreply.github.com>
2021-10-07 10:55:21 -0300

    Add files via upload

---
+++ Rastrigin_Function_SA.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def s_annealing(random_start, cost_function, random_neighbor, acceptance, temperature, maxsteps = 10000):
+
+    min_int = -5
+    max_int = 5
+    dim = 2
+
+    states = []
+    costs = []
+    
+    state = random_start(min_int, max_int, dim)
+    cost = cost_function(state)
+    states.append(state)
+    costs.append(cost)
+
+    for i in range(maxsteps):
+        fraction = i/float(maxsteps)
+        T = temperature(fraction)
+
+        new_state = random_neighbor(state, fraction, min_int, max_int, dim)
+        new_cost = cost_function(new_state)
+
+        if acceptance(cost, new_cost, T) > np.random.rand():
+            state, cost = new_state, new_cost
+            states.append(state)
+            costs.append(cost)
+    
+    return state, cost, states, costs;
+
+def random_start(mini, maxi, dim):
+    a, b = mini, maxi
+    s = []
+    s = a + (b - a)*np.random.rand(dim)
+    return s
+
+def cost_function(s):
+    return 10*2 + (s[0]**2 - 10*np.cos(2*np.pi*s[0])) + (s[1]**2 - 10*np.cos(2*np.pi*s[1]))
+
+def temperature(f):
+    return max(0.01, min(1, 1-f))
+
+def random_neighbor(s, fraction, mini, maxi, dim):
+    amplitude = (maxi - mini) * fraction / 10
+    delta = []
+    delta = (-amplitude/2.) + amplitude * np.random.rand(dim)
+    return clip(s + delta, mini, maxi)
+
+def clip(s, mini, maxi):
+    return max(mini, min(s[0], maxi)), max(mini, min(s[1], maxi))  
+
+def acceptance(c, new_c, t):
+    if new_c < c:
+        return 1
+    else:
+        return np.exp(-(new_c)/t)
+
+state_f_list= []
+c_f_list = []
+
+for runs in range(30):
+    state_f, c_f, states_f, costs_f = s_annealing(random_start, cost_function, random_neighbor, acceptance, temperature, maxsteps = 10000)
+    state_f_list.append(state_f_list)
+    c_f_list.append(c_f)
+
+
+plt.boxplot(c_f_list)
+plt.show()
---
+++ Rosenbrock_Function_GA.py
@@ -0,0 +1,98 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_population(size, dim, inf_boundary, sup_boundary):
+    population = []
+    for i in range(size):
+        population.append(inf_boundary + (sup_boundary - inf_boundary)*np.random.rand(dim))
+
+    return population
+
+def apply_function(individual):
+    return 100*(individual[1] - individual[0]**2)**2 + (1 - individual[0])**2
+
+def choice_by_roulette(sorted_population, fitness_sum):
+    offset = 0
+    normalized_fitness_sum = fitness_sum
+
+    lowest_fitness = apply_function(sorted_population[0])
+    if lowest_fitness < 0:
+        offset = -lowest_fitness;
+        normalized_fitness_sum += offset*len(sorted_population)
+
+    draw = np.random.rand()
+
+    accumulated = 0
+    for individual in sorted_population:
+        fitness = apply_function(individual) + offset
+        probability = fitness/normalized_fitness_sum
+        probability = 1 - probability
+        accumulated += probability
+
+        if draw < accumulated:
+            return individual
+
+
+def sort_population_by_fitness(population):
+    return sorted(population, key=apply_function)
+
+def crossover(individual_a, individual_b):
+    n = len(individual_a)
+    individual_c = np.zeros(n)
+
+    for i in range(n):
+        individual_c[i] = (individual_a[i] + individual_b[i])/2
+
+    return individual_c
+
+def mutation(individual, inf_boundary, sup_boundary):
+
+    n = len(individual)
+
+    for i in range(n):
+        draw = np.random.rand()
+        if draw > 0.5:
+            individual[i] += -0.05 + 0.1*np.random.rand()
+            individual[i] = min(max(individual[i], inf_boundary), sup_boundary)
+
+    return individual
+
+def make_next_generation(previous_population, inf_boundary, sup_boundary):
+
+    next_generation = []
+    sorted_population = sort_population_by_fitness(previous_population)
+    population_size = len(sorted_population)
+    fitness_sum = 0
+    
+    for i in range(population_size):
+        fitness_sum += apply_function(sorted_population[i])
+
+    for i in range(population_size):
+        first_individual = choice_by_roulette(sorted_population, fitness_sum)
+        second_individual = choice_by_roulette(sorted_population, fitness_sum)
+
+        new_individual = crossover(first_individual, second_individual)
+        next_generation.append(mutation(new_individual, inf_boundary, sup_boundary))
+
+    return next_generation
+
+fitness_state_list = []
+
+for runs in range(30):
+    generations = 100
+    size = 20
+    dim = 2
+    linf = -2
+    lsup = 2
+
+    population = generate_population(size, dim, linf, lsup)
+
+    for generation in range(generations):
+        
+        population = make_next_generation(population, linf, lsup)
+
+    best_individual = sort_population_by_fitness(population)[-1]
+    fitness_state_list.append(apply_function(best_individual))
+
+plt.boxplot(fitness_state_list)
+plt.show()
